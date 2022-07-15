58923fd2c5d8f986365a5602da022b98cb8ad74f
akinapellikarthik <karthik.akinapelli@gmail.com>
2022-07-15 09:51:54 +0530

    initial commit

---
+++ KMartAppBackend-1/src/main/java/com/kmart/demo/KmartDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.kmart.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmartDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmartDemoAppApplication.class, args);
+	}
+
+}
---
+++ KMartAppBackend-1/src/test/java/com/kmart/demo/KmartDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmart.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmartDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
