b033dad38f3e5c0a4834b83f5c2897f9858a7958
akinapellikarthik <karthik.akinapelli@gmail.com>
2022-07-18 21:36:04 +0530

    json include non null

--- KMartAppBackend-1/src/main/java/com/kmart/demo/dto/Product.java
+++ KMartAppBackend-1/src/main/java/com/kmart/demo/dto/Product.java
@@ -1,6 +1,7 @@
 package com.kmart.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @EqualsAndHashCode
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Product {
     private String name;
     private Float price;
