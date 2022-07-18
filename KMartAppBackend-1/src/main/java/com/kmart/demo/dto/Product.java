4b3e40b658e82c66738bfb6c76a792c9eb3e237e
akinapellikarthik <karthik.akinapelli@gmail.com>
2022-07-18 21:34:19 +0530

    added code in ProductType enum

--- KMartAppBackend-1/src/main/java/com/kmart/demo/dto/Product.java
+++ KMartAppBackend-1/src/main/java/com/kmart/demo/dto/Product.java
@@ -1,12 +1,20 @@
 package com.kmart.demo.dto;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Product {
     private String name;
     private Float price;
--- KMartAppBackend-1/src/main/java/com/kmart/demo/dto/ProductType.java
+++ KMartAppBackend-1/src/main/java/com/kmart/demo/dto/ProductType.java
@@ -2,4 +2,15 @@ package com.kmart.demo.dto;
 
 public enum ProductType {
 
+    PACKED_FOODS,
+    DETERGENT,
+    LOOSE_FOODS,
+    BREAD,
+    DRIED,
+    MEAT_FISH,
+    SNACKS,
+    BABY_CARE,
+    VEGETABLES,
+    FRUITS,
+    
 }
